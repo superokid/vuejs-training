10574af20c046de6e6f49577d8fc269e3d48e810
Kelvin Hong <kelvinok52@gmail.com>
2017-12-24 18:07:38 +0700

    ex8 component communication setup

---
+++ ex8-component-communication/src/components/UserDetail.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User name: {{ switchName() }}</p>
+        <button @click="resetName">Reset Name</button>
+    </div>
+</template>
+
+<script>
+	export default {
+		props: {
+			myName: {
+				type: String
+				// default: 'Max'
+				// type: Object,
+				// default: function(){
+				// 	return {
+				// 		name: 'Max'
+				// 	}
+				// }
+			},
+			resetFn: Function
+		},
+		methods: {
+			switchName(){
+				return this.myName.split("").reverse().join("");
+			},
+			resetName(){
+				this.myName = 'Max';
+				this.$emit('nameWasReset', this.myName);
+			}
+		}
+	}
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
