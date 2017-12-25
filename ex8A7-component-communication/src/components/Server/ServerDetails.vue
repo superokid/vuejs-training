d55ebcca0551be198e862383e46a638f0ce69438
Kelvin Hong <kelvinok52@gmail.com>
2017-12-25 14:16:33 +0700

    ex8/9 component communication

---
+++ ex8A7-component-communication/src/components/Server/ServerDetails.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p>Server Details are currently not updated</p>
+        <p v-if="!server">Please select a Server</p>
+        <p v-else>Server #{{ server.id }}, Status: {{ server.status }}</p>
+        <button @click="resetServer">reset server</button>
+    </div>
+</template>
+
+<script>
+	import { serverBus } from '../../main';
+
+	export default {
+		data: function() {
+			return {
+				server: null
+			}
+		},
+		methods: {
+			resetServer() {
+				this.server.status = 'Normal';
+			}
+		},
+		created() {
+			serverBus.$on('serverSelected', (server) => {
+				this.server = server;
+			});
+		}
+	}
+</script>
+
+<style>
+
+</style>
