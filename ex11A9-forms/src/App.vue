489e419a95b6702808111715a15d481f0741e5ff
Kelvin Hong <kelvinok52@gmail.com>
2017-12-25 18:36:37 +0700

    forms finished

--- ex11A9-forms/src/App.vue
+++ ex11A9-forms/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <form>
+        <form v-if="!submitted">
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <!-- Exercise 1 -->
@@ -9,6 +9,36 @@
                     <!-- Mail -->
                     <!-- Password -->
                     <!-- Store Data? Yes/No -->
+                    <app-full-name v-model="fullName"></app-full-name>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input
+                                type="email"
+                                class="form-control"
+                                v-model.lazy="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">password</label>
+                        <input
+                                type="password"
+                                class="form-control"
+                                v-model.lazy="password">
+                    </div>
+                    <div class="form-group">
+                        <p>Store Data?</p>
+                        <label>
+                            <input type="radio" value="Yes" v-model="storeData"> Yes
+                        </label>
+                        <label>
+                            <input type="radio" value="No" v-model="storeData"> No
+                        </label>
+                    </div>
+
+                    <button
+                            type="submit"
+                            class="btn btn-primary"
+                            @click.prevent="submitted = true">Submit!
+                    </button>
 
                     <!-- Exercise 2 -->
                     <!-- Only display the Form if it has NOT been submitted -->
@@ -21,17 +51,17 @@
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="submitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{ fullName }}</p>
+                        <p>Mail: {{ email }}</p>
+                        <p>Password: {{ password }}</p>
+                        <p>Store in Database?: {{ storeData }}</p>
                     </div>
                 </div>
             </div>
@@ -40,7 +70,21 @@
 </template>
 
 <script>
+    import FullName from './FullName.vue';
+
     export default {
+        data() {
+            return {
+                fullName: 'Supper ok',
+                email: '',
+                password: '',
+                storeData: 'Yes',
+                submitted: false
+            }
+        },
+        components: {
+            appFullName: FullName
+        }
     }
 </script>
 
