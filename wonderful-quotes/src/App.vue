95add3cb79adfdda3e709175cb2d2f2bb168623f
Kelvin Hong <kelvinok52@gmail.com>
2017-12-25 16:22:56 +0700

    wonderful-quotes finish up

--- wonderful-quotes/src/App.vue
+++ wonderful-quotes/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-    	<app-header></app-header>
+    	<app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
     	<app-newQuote @quoteAdded="newQuote"></app-newQuote>
     	<app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
     	<div class="row">
@@ -27,6 +27,9 @@
         },
         methods:{
         	newQuote(quote) {
+        		if(this.quotes.length >= this.maxQuotes){
+        			return alert('Please delete Quote first!');
+        		}
         		this.quotes.push(quote);
         	},
         	deleteQuote(index) {
