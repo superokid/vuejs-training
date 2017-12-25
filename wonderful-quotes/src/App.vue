c8c0082e5af7da617d3a2537c8b7ec81a25fdd14
Kelvin Hong <kelvinok52@gmail.com>
2017-12-25 16:09:34 +0700

    wonderful-quotes, addremove and setup

---
+++ wonderful-quotes/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+    	<app-header></app-header>
+    	<app-newQuote @quoteAdded="newQuote"></app-newQuote>
+    	<app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    	<div class="row">
+    		<div class="col-md-12 text-center">
+    			<div class="alert alert-info">Info: Click on the Quote to Delete it!</div>
+    		</div>
+    	</div>
+    </div>
+</template>
+
+<script>
+	import QuoteGrid from './components/QuoteGrid.vue';
+	import NewQuote from './components/NewQuote.vue';
+	import Header from './components/Header.vue';
+
+    export default {
+        data: function() {
+        	return {
+        		quotes: [
+        			'Just a Quote to see something'
+        		],
+        		maxQuotes: 10
+        	}
+        },
+        methods:{
+        	newQuote(quote) {
+        		this.quotes.push(quote);
+        	},
+        	deleteQuote(index) {
+        		this.quotes.splice(index, 1);
+        	}
+        },
+        components: {
+        	appQuoteGrid: QuoteGrid,
+        	appNewQuote: NewQuote,
+        	appHeader: Header
+        }
+    }
+</script>
+
+<style>
+</style>
