17bd510f66eadc9662e60a80242769518f1ae97d
Kelvin Hong <kelvinok52@gmail.com>
2017-12-30 11:21:15 +0700

    ex16-routing hash and animation

--- ex16-routing/src/App.vue
+++ ex16-routing/src/App.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-sm-offset-2 col-md-12 col-md-offset-3">
+            <div class="col-sm-offset-2 col-md-9 col-md-offset-3">
                 <h1>Routing</h1>
             </div>
             <hr>
-            <app-header></app-header>
-            <router-view></router-view>
+            <router-view name="header-top"></router-view>
+            <transition name="slide" mode="out-in">
+                <router-view></router-view>
+            </transition>
+            <router-view name="header-bottom"></router-view>
         </div>
     </div>
 </template>
@@ -21,4 +24,36 @@
 </script>
 
 <style>
+    .slide-leave-active {
+        transition: opacity 1s ease;
+        opacity: 0;
+        animation: slide-out 1s ease-out forwards;
+    }
+
+    .slide-leave {
+        opacity: 1;
+        transform: translateX(0);
+    }
+
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
+
+    @keyframes slide-out {
+        0% {
+            transform: translateY(0);
+        }
+        100% {
+            transform: translateY(-30px);
+        }
+    }
+
+    @keyframes slide-in {
+        0% {
+            transform: translateY(-30px);
+        }
+        100%{
+            transform: translateY(0);
+        }
+    }
 </style>
