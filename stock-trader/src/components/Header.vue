71be74dd491dacaff7c85a3ed054f6af156b5707
Kelvin Hong <kelvinok52@gmail.com>
2018-01-07 21:25:53 +0700

    authentication finish

--- stock-trader/src/components/Header.vue
+++ stock-trader/src/components/Header.vue
@@ -1,43 +1,4 @@
 <template>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	  <router-link to="/" class="navbar-brand">Stock Trader</router-link>
-
-	  <div class="collapse navbar-collapse">
-	    <ul class="navbar-nav mr-auto">
-	      <li class="nav-item">
-	      	<router-link to="portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
-	      	<router-link to="stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
-	        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-	      </li>
-	      <li class="nav-item dropdown">
-	        <a 
-	        	class="nav-link dropdown-toggle" 
-	        	href="#" 
-	        	id="navbarDropdown" 
-	        	role="button" 
-	        	data-toggle="dropdown" 
-	        	aria-haspopup="true" 
-	        	aria-expanded="false">
-	         	Save & Load
-	        </a>
-	        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-	          <a class="dropdown-item" href="#">Save Data</a>
-	          <a class="dropdown-item" href="#">Load Data</a>
-	        </div>
-	      </li>
-	      <li class="nav-item">
-	        <a class="nav-link" href="#">Link</a>
-	      </li>
-	      
-	      <li class="nav-item">
-	        <a class="nav-link disabled" href="#">Disabled</a>
-	      </li>
-	    </ul>
-	  </div>
-	</nav>
-</template>
-
-<template>
 	<nav class="navbar navbar-default">
 	  <div class="container-fluid">
 	    <!-- Brand and toggle get grouped for better mobile display -->
